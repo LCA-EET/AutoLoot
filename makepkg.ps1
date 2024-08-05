dc4cc387b0123a85e8fd394fe445917b2105be4d
LCA-EET <129810393+LCA-EET@users.noreply.github.com>
2024-08-04 21:48:49 -0400

    update

--- makepkg.ps1
+++ makepkg.ps1
@@ -21,6 +21,7 @@ foreach($folder in $folders){
 Copy-Item -Path ("bubb_lib.tph") -Destination $modPath 
 Copy-Item -Path ($tp2Name + ".tp2") -Destination $modPath 
 Copy-Item -Path "weidu.exe" -Destination ($basePath + "/" + $exePath)
+Copy-Item -Path "readme.md" -Destination ($basePath + "/ReadMe.md")
 
 $7zipPath = "$env:ProgramFiles/7-Zip/7z.exe"
 
---
+++ readme.md
@@ -0,0 +1,3 @@
+This mod extends Bubb's original auto loot mod to support EET compatibility. It is not currently compatible with other mods that make modifications to the game UI.
+
+Install the mod after any mods that add items to the game. For EET, install the mod before eet_end.
