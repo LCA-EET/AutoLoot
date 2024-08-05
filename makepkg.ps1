979e94d909267c35b0c02b6a517c97e398029824
LCA-EET <129810393+LCA-EET@users.noreply.github.com>
2024-08-04 21:42:54 -0400

    update

---
+++ makepkg.ps1
@@ -0,0 +1,41 @@
+$basePath = "AutoLoot"
+$tp2Name = "AutoLoot"
+$modPath = $basePath + "/" + $tp2Name 
+$archive = $basePath + ".zip"
+$exePath = "setup-" + $tp2Name + ".exe"
+$testDir = "F:\Baldur's Gate EE\00783\"
+$folders = @(
+'copy',
+'tra',
+'work'
+)
+
+Remove-Item -LiteralPath $modPath -Force -Recurse
+
+foreach($folder in $folders){
+	Copy-Item -Path $folder -Destination ($modPath + "/" + $folder) -Recurse	
+}
+
+& $PSScriptRoot/d_compactor.ps1 -dPath $modPath
+
+Copy-Item -Path ("bubb_lib.tph") -Destination $modPath 
+Copy-Item -Path ($tp2Name + ".tp2") -Destination $modPath 
+Copy-Item -Path "weidu.exe" -Destination ($basePath + "/" + $exePath)
+
+$7zipPath = "$env:ProgramFiles/7-Zip/7z.exe"
+
+if (-not (Test-Path -Path $7zipPath -PathType Leaf)) {
+	$7zipPath = "F:/Program Files/7-Zip/7z.exe"
+}
+
+Set-Alias Start-SevenZip $7zipPath
+
+$Source = "./" + $basePath + "/*"
+$Target = "./" + $archive
+
+Start-SevenZip a -mx=9 $Target $Source
+
+Remove-Item -LiteralPath $basePath -Force -Recurse
+Get-FileHash $archive -Algorithm SHA256 > SHA256.txt
+
+Copy-Item -Path $archive -Destination ("\\192.168.1.88\smbuser\Home\Installers\" + $archive)
